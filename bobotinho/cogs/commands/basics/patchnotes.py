2656d5507612fb69466b035daf547ae94973dd32
Leandro <ccleandroc@gmail.com>
2021-05-23 11:46:53 -0300

    🆕 "patchnotes" -> "news"

--- bobotinho/cogs/commands/basics/patchnotes.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-description = "Receba o link das notas de atualização do bot"
-aliases = ["pn"]
-
-
-async def func(ctx):
-    ctx.response = f"veja as notas de atualização: {ctx.bot.site}/patch-notes"
