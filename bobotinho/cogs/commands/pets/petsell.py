2f0b9716bcdb19ec628e42c6525f4e6776734fa3
Leandro <ccleandroc@gmail.com>
2021-05-31 09:30:42 -0300

    🔧 Hot fix: pet sell

--- bobotinho/cogs/commands/pets/petsell.py
+++ bobotinho/cogs/commands/pets/petsell.py
@@ -7,7 +7,7 @@ usage = "digite o comando e o nome ou espécie do pet que quer devolver"
 
 
 async def func(ctx, *, content: str):
-    if pet := await models.Pet.find(user=ctx.author.name, name=content.title(), specie=content.lower()):
+    if pet := await models.Pet.find(id=ctx.author.id, name=content.title(), specie=content.lower()):
         price = round(P.all_pets[pet.specie].price * 0.7)
         emoji = P.all_pets[pet.specie].emoji
         cookie = await models.Cookie.get(id=ctx.author.id)
