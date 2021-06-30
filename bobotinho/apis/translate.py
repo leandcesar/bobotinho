36e0203418ba97dc3c6d73cccd868ac74cb5bd24
leandcesar <ccleandroc@gmail.com>
2021-06-30 20:29:50 -0300

    🗣️  Implement Translator instead of import lib

--- bobotinho/apis/translate.py
+++ bobotinho/apis/translate.py
@@ -1,4 +1,79 @@
 # -*- coding: utf-8 -*-
-from google_trans_new import google_translator
+import json
+import requests
+import urllib3
+from urllib.parse import quote
 
-TranslateApi = google_translator()
+FILENAME = "bobotinho//data//languages.json"
+
+with open(FILENAME, "r", encoding="utf-8") as file:
+    LANGUAGES = json.load(file)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class TranslateApi:
+    timeout = 5
+    proxies = {}
+    url = "https://translate.google.cn/_/TranslateWebserverUi/data/batchexecute"
+    headers = {
+        "Referer": "https://translate.google.cn",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+
+    @staticmethod
+    def _package_rpc(text: str, lang_src: str = "auto", lang_tgt: str = "auto"):
+        escaped_parameter = json.dumps([[text.strip(), lang_src, lang_tgt, True], [1]], separators=(",", ":"))
+        espaced_rpc = json.dumps([[["MkEWBc", escaped_parameter, None, "generic"]]], separators=(",", ":"))
+        return f"f.req={quote(espaced_rpc)}&"
+
+    @classmethod
+    def translate(cls, text: str, lang_tgt: str = "auto", lang_src: str = "auto"):
+        lang_src = lang_src if LANGUAGES.get(lang_src) else "auto"
+        lang_src = lang_src if LANGUAGES.get(lang_tgt) else "auto"
+        response = requests.Request(
+            method="POST",
+            url=cls.url,
+            data=cls._package_rpc(text, lang_src, lang_tgt),
+            headers=cls.headers,
+        )
+        with requests.Session() as s:
+            s.proxies = cls.proxies
+            r = s.send(request=response.prepare(), verify=False, timeout=cls.timeout)
+        for line in r.iter_lines(chunk_size=1024):
+            decoded_line = line.decode("utf-8")
+            if "MkEWBc" in decoded_line:
+                _response = list(json.loads(list(json.loads(decoded_line))[0][2]))
+                response = _response[1][0]
+                if len(response) == 1:
+                    if len(response[0]) <= 5:
+                        translate_text = response[0][0]
+                        return translate_text
+                    translate_text = ""
+                    sentences = response[0][5]
+                    for sentence in sentences:
+                        translate_text += sentence[0].strip() + " "
+                    return translate_text
+                elif len(response) == 2:
+                    print("??")
+                    return [sentences.append(i[0]) for i in response]
+
+    @classmethod
+    def detect(cls, text: str):
+        response = requests.Request(
+            method="POST",
+            url=cls.url,
+            data=cls._package_rpc(text),
+            headers=cls.headers,
+        )
+        with requests.Session() as s:
+            s.proxies = cls.proxies
+            r = s.send(request=response.prepare(), verify=False, timeout=cls.timeout)
+        for line in r.iter_lines(chunk_size=1024):
+            decoded_line = line.decode("utf-8")
+            if "MkEWBc" in decoded_line:
+                _response = list(json.loads(list(json.loads(decoded_line))[0][2]))
+                return _response[0][2]
--- bobotinho/cogs/commands/tools/translate.py
+++ bobotinho/cogs/commands/tools/translate.py
@@ -5,7 +5,6 @@ from bobotinho.apis import translate
 from bobotinho.logger import log
 
 description = "Saiba a tradução de alguma mensagem"
-aliases = ["tl"]
 usage = "digite o comando e um texto para ser traduzido"
 
 
@@ -16,14 +15,13 @@ async def func(ctx, arg: str, *, content: str = ""):
     else:
         content = f"{arg} {content}"
     src = src if src else "auto"
-    dest = dest if dest else "en" if translate.TranslateApi.detect(content)[0] == "pt" else "pt"
+    dest = dest if dest else "en" if translate.TranslateApi.detect(content) == "pt" else "pt"
     try:
         translation = translate.TranslateApi.translate(content, dest, src)
     except Exception as e:
         log.exception(e)
         translation = None
     if translation and translation != content:
-        # TODO: se a tradução for uma banword... vish
         ctx.response = f"{src}->{dest}: {translation}"
     else:
         ctx.response = "não foi possível traduzir isso"
