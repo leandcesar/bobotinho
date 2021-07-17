cebfccb549764b714cc9bd46a72beb589cf29560
leandcesar <ccleandroc@gmail.com>
2021-07-17 11:08:18 -0300

    🔧 Fix daily reset

--- bobotinho/cogs/tasks/reset_daily.py
+++ bobotinho/cogs/tasks/reset_daily.py
@@ -14,10 +14,11 @@ async def func(bot) -> None:
         if now.hour > 9:
             target += timedelta(days=1)
         delta = (target - now).total_seconds()
-        await asyncio.sleep(delta, loop=bot.loop)
+        if os.getenv("RESETTING_DAILY") == "0":
+            await asyncio.sleep(delta, loop=bot.loop)
         log.info("Resetting daily cookies...")
         os.environ["RESETTING_DAILY"] = "1"
-        sponsors = await models.User.filter(sponsors=True).all().values_list("id", flat=True)
+        sponsors = await models.User.filter(sponsor=True).all().values_list("id", flat=True)
         await models.Cookie.filter(id__in=sponsors).update(daily=2)
         await models.Cookie.filter(id__not_in=sponsors, daily=0).update(daily=1)
         log.info("Daily cookies reset")
