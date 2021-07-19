4b8fcf86862e62ac78e9adf00398324a41e0a7f8
leandcesar <ccleandroc@gmail.com>
2021-07-19 15:58:45 -0300

    📛 Fix multiple badges bug

--- bobotinho/cogs/commands/preferences/badge.py
+++ bobotinho/cogs/commands/preferences/badge.py
@@ -2,12 +2,13 @@
 from bobotinho.utils import checks, convert
 
 description = "Defina a sua badge de apoiador"
-usage = 'digite o comando e o emoji que quiser usar como badge no bot'
+usage = 'digite o comando e um emoji que quiser usar como badge no bot'
 extra_checks = [checks.sponsor]
 
 
 async def func(ctx, arg: str):
-    if convert.emoji2str(arg) != arg:
+    emoji = convert.emoji2str(arg)
+    if emoji != arg and emoji.count(":") == 2:
         ctx.user.badge = arg
         await ctx.user.save()
         ctx.response = "você alterou sua badge de apoiador"
