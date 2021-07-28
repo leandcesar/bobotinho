3601626bdddc37b998b9d4e2937ee3c8b2cb4e65
leandcesar <ccleandroc@gmail.com>
2021-07-28 19:48:51 -0300

    💡 Refactor suggestions webhook

--- bobotinho/webhook.py
+++ bobotinho/webhook.py
@@ -6,20 +6,14 @@ from bobotinho import aiorequests
 
 class Webhook:
     url = os.getenv("BOT_NOTIFIER_URL")
-    app = "bobotinho-bot"
     timestamp_format = "%Y-%m-%dT%H:%M:%SZ"
 
     @classmethod
-    async def suggestions(cls, suggest) -> None:
+    async def send(cls, resource, **kwargs) -> None:
         if not cls.url:
             return
         data = {
-            "app": cls.app,
-            "resource": "suggestions",
-            "id": suggest.id,
-            "content": suggest.content,
-            "author": suggest.name,
-            "channel": suggest.channel,
-            "timestamp": suggest.updated_at.strftime(cls.timestamp_format),
+            "resource": resource,
+            "data": kwargs,
         }
         await aiorequests.post(cls.url, json=data, wait_response=False)
