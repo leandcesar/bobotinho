08358d1a453c085f43d5fd5d6a81927b1d50c990
leandcesar <ccleandroc@gmail.com>
2021-07-28 20:10:09 -0300

    🐛 Add 'bug' command

---
+++ bobotinho/cogs/commands/basics/bug.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from bobotinho.database import models
+
+description = "Reporte um bug que está ocorrendo no Bot"
+usage = "digite o comando e o bug que você encontrou com o máximo de contexto possível"
+
+
+async def func(ctx, *, content: str):
+    bug = await models.Bug.create(
+        name=ctx.author.name,
+        channel=ctx.channel.name,
+        content=content
+    )
+    ctx.response = f"seu bug foi reportado 🐛 (ID {bug.id})"
