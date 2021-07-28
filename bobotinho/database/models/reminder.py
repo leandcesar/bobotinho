4416dd4bada69a2bc5187b9d3ea4a3eb3b148e3b
leandcesar <ccleandroc@gmail.com>
2021-07-28 20:47:16 -0300

    🗑️ Command to list and delete reminders

--- bobotinho/database/models/reminder.py
+++ bobotinho/database/models/reminder.py
@@ -3,9 +3,9 @@ from bobotinho.database.base import Base, TimestampMixin, ContentMixin, fields, 
 
 
 class Reminder(Base, TimestampMixin, ContentMixin):
-    from_user_id = fields.IntField()
-    to_user_id = fields.IntField()
-    channel_id = fields.IntField()
+    from_user = fields.ForeignKeyField("models.User", related_name="reminders_from")
+    to_user = fields.ForeignKeyField("models.User", related_name="reminders_to")
+    channel = fields.ForeignKeyField("models.User", related_name="reminders_channel")
     scheduled_for = fields.DatetimeField(null=True)
 
     class Meta:
