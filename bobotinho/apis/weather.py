fe2038d2ebc9aa5f43a2876b5ee77875c5682215
leandcesar <ccleandroc@gmail.com>
2021-09-07 19:24:47 -0300

    🌤️ Update PyOWM

--- bobotinho/apis/weather.py
+++ bobotinho/apis/weather.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 from pyowm import OWM
+from pyowm.utils.config import get_default_config
+
 from bobotinho import config
 
-Weather = OWM(config.weather_key, language="pt")
+
+config_dict = get_default_config()
+config_dict["language"] = "pt_br"
+
+owm = OWM(config.weather_key, config_dict)
+Weather = owm.weather_manager()
--- bobotinho/cogs/tools/commands/weather.py
+++ bobotinho/cogs/tools/commands/weather.py
@@ -6,24 +6,26 @@ aliases = ["wt"]
 usage = "digite o comando e o nome de um local para saber o clima"
 
 
-async def command(ctx, *, content: str = ""):
+async def command(ctx, *, content: str = None):
     if not content:
         place = ctx.user.city
     else:
-        place = content.lower()
+        place = content
     if place in ("salvador", "socorro", "santiago"):
         place = f"{place}, br"
     try:
         observation = Weather.weather_at_place(place)
-        city = observation.get_location().get_name()
-        country = observation.get_location().get_country()
-        status = observation.get_weather().get_detailed_status()
-        temp = observation.get_weather().get_temperature("celsius")["temp"]
-        wind = observation.get_weather().get_wind()["speed"]
-        humidiy = observation.get_weather().get_humidity()
+        city = observation.location.name
+        country = observation.location.country
+        status = observation.weather.detailed_status
+        temperature = observation.weather.temperature("celsius")
+        temp = temperature["temp"]
+        feels_like = temperature["feels_like"]
+        wind = observation.weather.wind()["speed"]
+        humidiy = observation.weather.humidity
         ctx.response = (
-            f"em {city} ({country}): {status}, {temp}°C, "
-            f"ventos a {wind}m/s e {humidiy}% de umidade"
+            f"em {city} ({country}): {status}, {temp}°C (sensação de "
+            f"{feels_like}°C), ventos a {wind}m/s e {humidiy}% de umidade"
         )
     except AssertionError:
         ctx.response = "digite o comando e o nome de um local para saber o clima"
