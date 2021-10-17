cdb26b20d872ecc67b4a4f57f265c4defb3b3a2c
leandcesar <ccleandroc@gmail.com>
2021-10-17 16:01:08 -0300

    🔨 TMDb refactor

--- bobotinho/cogs/tools/commands/movie.py
+++ bobotinho/cogs/tools/commands/movie.py
@@ -1,55 +1,88 @@
 # -*- coding: utf-8 -*-
 import random
+import re
 
-from bobotinho.apis import Movie
+from bobotinho.apis import Discover, Find, Movie, Trending
 
 description = "Receba informações ou sugestões de filme, e até onde assistí-lo"
 usage = "digite o comando e alguma tag: top, trend, popular, ou o nome de um filme"
 
+METHODS = {
+    "now": Movie.now_playing,
+    "upcoming": Movie.upcoming,
+    "popular": Movie.popular,
+    "top": Movie.top_rated,
+    "trend": Trending.movie_week,
+}
+
+PROVIDERS = {
+    "netflix": 8,
+    "prime": 119,
+    "disney+": 337,
+    "itunes": 2,
+    "google": 3,
+    "looke": 47,
+    "fox": 229,
+    "mubi": 11,
+    "star+": 619,
+    "paramount+": 531,
+    "hbo": 384,
+    "claro": 167,
+    "telecine": 227,
+    "globo": 307,
+}
+
+
+def find_movie(query: str):
+    if not query:
+        return None
+    try:
+        imdb_id = re.search(r"(?:imdb.com\/title\/)?(tt[0-9]{7})", query)[1]
+    except TypeError:
+        movie = Movie.search(query)[0]
+    else:
+        movie = Find.find_by_imdb_id(imdb_id)["movie_results"][0]
+    finally:
+        return movie
+
+
+def discover_movie(provider: str):
+    try:
+        movies = Discover.movies({"with_watch_providers": PROVIDERS[provider]})
+    except KeyError:
+        movies = Discover.movies({})
+    finally:
+        return movies
+
 
 async def command(ctx, arg: str, *, content: str = ""):
     arg = arg.lower()
-    if arg in ("now", "upcoming", "latest", "popular", "top", "trend"):
-        movie = await getattr(Movie, arg)()
+    content = content.lower()
+    if not content and arg in METHODS:
+        movies = METHODS[arg]()
     elif arg == "discover":
-        provider = Movie.main_providers.get(content.lower())
-        movie = await Movie.discover(provider)
-    elif arg in ("similar", "recommendation"):
-        movie = await Movie.find(content)
-        if movie.get("id"):
-            movie = await getattr(Movie, arg)(movie["id"])
-        else:
-            movie = {}
+        movies = discover_movie(content)
+    elif arg == "similar":
+        movie = find_movie(content)
+        movies = Movie.similar(movie.id) if movie else []
+    elif arg == "recommendation":
+        movie = find_movie(content)
+        movies = Movie.recommendations(movie.id) if movie else []
     else:
-        content = f"{arg} {content}"
-        movie = await Movie.find(content)
-        movie.pop("genre_ids", None)
+        movies = find_movie(f"{arg} {content}")
 
-    if movie.get("results"):
-        movie = random.sample(movie["results"], 1)[0]
-
-    if movie.get("id"):
-        id = movie["id"]
+    try:
+        movie = random.sample(movies, 1)[0] if isinstance(movies, list) else movies
+        movie_id = movie["id"]
         title = movie["title"]
         year = movie["release_date"][:4]
-
+        providers = Movie.providers(movie_id)
+        ctx.response = f"veja o filme: {title} ({year})"
         try:
-            genre_id = movie["genre_ids"][0]
-            quote = Movie.genre_id_to_quote[genre_id]
+            providers_br = providers["results"]["BR"]["flatrate"]
+            providers_br = ", ".join([provider["provider_name"] for provider in providers_br])
+            ctx.response += f", disponível em {providers_br}"
         except KeyError:
-            quote = None
-
-        try:
-            watch = await Movie.watch(id)
-            providers = ", ".join([p["provider_name"] for p in watch["results"]["BR"]["flatrate"]])
-        except KeyError:
-            providers = None
-
-        if quote:
-            ctx.response = f"{quote} {title} ({year})"
-        else:
-            ctx.response = f"veja o filme: {title} ({year})"
-        if providers:
-            ctx.response += f", disponível em {providers}"
-    else:
-        ctx.response = "não encontrei... se não estiver conseguindo, tente me enviar o link do IMDb"
+            pass
+    except Exception:
+        ctx.response = "não encontrei o filme... confira se escreveu corretamente ou me envie o link do IMDb"
--- bobotinho/cogs/tools/commands/tv.py
+++ bobotinho/cogs/tools/commands/tv.py
@@ -1,55 +1,86 @@
 # -*- coding: utf-8 -*-
 import random
+import re
 
-from bobotinho.apis import Tv
+from bobotinho.apis import Discover, Find, Trending, TV
 
 description = "Receba informações ou sugestões de uma série, e até onde assistí-la"
 usage = "digite o comando e alguma tag: top, trend, popular, ou o nome de uma série"
 
+METHODS = {
+    "now": TV.on_the_air,
+    "popular": TV.popular,
+    "top": TV.top_rated,
+    "trend": Trending.tv_week,
+}
+
+PROVIDERS = {
+    "netflix": 8,
+    "prime": 119,
+    "disney+": 337,
+    "looke": 47,
+    "fox": 229,
+    "star+": 619,
+    "paramount+": 531,
+    "hbo": 384,
+    "crunchyroll": 283,
+    "claro": 167,
+    "globo": 307,
+}
+
+
+def find_tv_show(query: str):
+    if not query:
+        return None
+    try:
+        imdb_id = re.search(r"(?:imdb.com\/title\/)?(tt[0-9]{7})", query)[1]
+    except TypeError:
+        tv_show = TV.search(query)[0]
+    else:
+        tv_show = Find.find_by_imdb_id(imdb_id)["tv_results"][0]
+    finally:
+        return tv_show
+
+
+def discover_tv_show(provider: str):
+    try:
+        tv_shows = Discover.tv_shows({"with_watch_providers": PROVIDERS[provider]})
+    except KeyError:
+        tv_shows = Discover.tv_shows({})
+    finally:
+        return tv_shows
+
 
 async def command(ctx, arg: str, *, content: str = ""):
     arg = arg.lower()
-    if arg in ("now", "latest", "popular", "top", "trend"):
-        tv = await getattr(Tv, arg)()
+    content = content.lower()
+    if not content and arg in METHODS:
+        tv_shows = METHODS[arg]()
     elif arg == "discover":
-        provider = Tv.main_providers.get(content.lower())
-        tv = await Tv.discover(provider)
-    elif arg in ("similar", "recommendation"):
-        tv = await Tv.find(content)
-        if tv.get("id"):
-            tv = await getattr(Tv, arg)(tv["id"])
-        else:
-            tv = {}
+        tv_shows = discover_tv_show(content)
+    elif arg == "similar":
+        tv_show = find_tv_show(content)
+        tv_shows = TV.similar(tv_show.id) if tv_show else []
+    elif arg == "recommendation":
+        tv_show = find_tv_show(content)
+        tv_shows = TV.recommendations(tv_show.id) if tv_show else []
     else:
-        content = f"{arg} {content}"
-        tv = await Tv.find(content)
-        tv.pop("genre_ids", None)
+        tv_shows = find_tv_show(f"{arg} {content}")
 
-    if tv.get("results"):
-        tv = random.sample(tv["results"], 1)[0]
-
-    if tv.get("id"):
-        id = tv["id"]
-        title = tv["name"]
-        year = tv["first_air_date"][:4]
-
+    try:
+        print(tv_shows)
+        tv_show = random.sample(tv_shows, 1)[0] if isinstance(tv_shows, list) else tv_shows
+        print(tv_show)
+        tv_show_id = tv_show["id"]
+        title = tv_show["name"]
+        year = tv_show["first_air_date"][:4]
+        providers = TV.providers(tv_show_id)
+        ctx.response = f"veja a série: {title} ({year})"
         try:
-            genre_id = tv["genre_ids"][0]
-            quote = Tv.genre_id_to_quote[genre_id]
+            providers_br = providers["results"]["BR"]["flatrate"]
+            providers_br = ", ".join([provider["provider_name"] for provider in providers_br])
+            ctx.response += f", disponível em {providers_br}"
         except KeyError:
-            quote = None
-
-        try:
-            watch = await Tv.watch(id)
-            providers = ", ".join([p["provider_name"] for p in watch["results"]["BR"]["flatrate"]])
-        except KeyError:
-            providers = None
-
-        if quote:
-            ctx.response = f"{quote} {title} ({year})"
-        else:
-            ctx.response = f"veja a série: {title} ({year})"
-        if providers:
-            ctx.response += f", disponível em {providers}"
-    else:
-        ctx.response = "não encontrei... se não estiver conseguindo, tente me enviar o link do IMDb"
+            pass
+    except Exception:
+        ctx.response = "não encontrei a série... confira se escreveu corretamente ou me envie o link do IMDb"
