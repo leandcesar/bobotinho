14317259e380d1f2c87936f2397b93b4707d3caf
Leandro César <ccleandroc@gmail.com>
2022-02-10 20:16:35 -0300

    Refactor cache

--- bobotinho/cache.py
+++ bobotinho/cache.py
@@ -2,7 +2,6 @@
 import time
 from collections import OrderedDict
 from threading import RLock
-from typing import Any, Optional
 
 
 class TTLOrderedDict(OrderedDict):
@@ -12,11 +11,11 @@ class TTLOrderedDict(OrderedDict):
 
     def _expired(self, key: str, now: float) -> bool:
         with self._lock:
-            expire: Optional[float] = super().__getitem__(key)[0]
+            expire = super().__getitem__(key)[0]
             return expire and expire < now
 
     def _purge(self) -> None:
-        now: float = time.time()
+        now = time.time()
         [
             self.__delitem__(key)
             for key in [
@@ -24,44 +23,42 @@ class TTLOrderedDict(OrderedDict):
             ]
         ]
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key: str, value: str) -> None:
         with self._lock:
-            expire: Optional[float] = None
+            expire = None
             super().__setitem__(key, (expire, value))
 
     def __delitem__(self, key: str) -> None:
         with self._lock:
             super().__delitem__(key)
 
-    def __getitem__(self, key: str) -> Any:
+    def __getitem__(self, key: str) -> str:
         with self._lock:
-            now: float = time.time()
+            now = time.time()
             if self._expired(key, now):
                 self.__delitem__(key)
                 raise KeyError
             return super().__getitem__(key)[1]
-
     def keys(self, pattern: str = None) -> list:
         with self._lock:
             self._purge()
             return super().keys()
-
-    def set(self, key: str, value: Any, ex: int = None, nx: bool = None) -> bool:
+    def set(self, key: str, value: str, ex: int = None, nx: bool = None) -> bool:
         if nx and self.get(key):
             return False
         with self._lock:
             self._purge()
-            expire: Optional[float] = time.time() + ex if ex else None
+            expire = time.time() + ex if ex else None
             super().__setitem__(key, (expire, value))
             return True
 
-    def get(self, key: str, default: Any = None) -> Any:
+    def get(self, key: str, default: str = None) -> str:
         try:
             return self[key]
         except KeyError:
             return default
 
-    def getset(self, key: str, value: Any) -> Any:
+    def getset(self, key: str, value: str) -> str:
         current_value = self.get(key)
         self.set(key, value)
         return current_value
