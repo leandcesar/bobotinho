c8f793f6340b1b6ac22f892f107b92b364e6defb
Leandro César <ccleandroc@gmail.com>
2022-02-10 19:22:52 -0300

    Refactor database

--- bobotinho/__main__.py
+++ bobotinho/__main__.py
@@ -1,44 +1,32 @@
 # -*- coding: utf-8 -*-
 import sys
+from redis import Redis
 
-from bobotinho import config, log
+from bobotinho import config, database, log
+from bobotinho.bots import TwitchBot
+from bobotinho.cache import TTLOrderedDict
 
-try:
-    from bobotinho.database import Database
-
-    db: Database = Database(config.database_url)
-except Exception as e:
-    log.critical(e, exc_info=1)
-    sys.exit("[CRITICAL] Database constructor failure")
-
-try:
-    from bobotinho.bots import TwitchBot
-
-    bot: TwitchBot = TwitchBot(config)
-    bot.load_cogs()
-except Exception as e:
-    log.critical(e, exc_info=1)
-    sys.exit("[CRITICAL] Twitch Bot constructor failure")
-
-try:
-    from redis import Redis
-
-    bot.cache = Redis.from_url(config.redis_url, encoding="utf-8", decode_responses=True)
-except Exception as e:
-    from bobotinho.cache import TTLOrderedDict
-
-    log.warning(e)
-    bot.cache = TTLOrderedDict()
 
 if __name__ == "__main__":
     try:
-        bot.loop.run_until_complete(db.init())
+        bot = TwitchBot(config)
+        bot.load_cogs()
+    except Exception as e:
+        log.critical(e, exc_info=1)
+        sys.exit("[CRITICAL] Twitch Bot constructor failure")
+    try:
+        bot.cache = Redis.from_url(config.redis_url, encoding="utf-8", decode_responses=True)
+    except Exception as e:
+        log.warning(e)
+        bot.cache = TTLOrderedDict()
+    try:
+        bot.loop.run_until_complete(database.init(config.database.url))
         bot.loop.run_until_complete(bot.start())
         bot.loop.run_forever()
     except BaseException as e:
         log.exception(e, extra={"locals": locals()})
     finally:
-        bot.loop.run_until_complete(db.close())
         bot.cache.close()
+        bot.loop.run_until_complete(database.close())
         bot.loop.run_until_complete(bot.stop())
         bot.loop.close()
--- bobotinho/database/__init__.py
+++ bobotinho/database/__init__.py
@@ -2,14 +2,11 @@
 from tortoise import Tortoise
 
 
-class Database:
-    def __init__(self, url: str):
-        self.url = url
+async def init(database_url: str, *, models_path: str = "bobotinho.database.models") -> None:
+    models = [models_path]
+    await Tortoise.init(db_url=database_url, modules={"models": models})
+    await Tortoise.generate_schemas(safe=True)
 
-    async def init(self):
-        models = ["bobotinho.database.models"]
-        await Tortoise.init(db_url=self.url, modules={"models": models})
-        await Tortoise.generate_schemas(safe=True)
 
-    async def close(self):
-        await Tortoise.close_connections()
+async def close() -> None:
+    await Tortoise.close_connections()
